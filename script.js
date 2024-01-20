de76c997d7e582e07ae2b5cccde211f287e09947
William9529 <140195303+William9529@users.noreply.github.com>
2024-01-20 01:39:51 +0100

    nagyjából oké

--- script.js
+++ script.js
@@ -1,1 +1,43 @@
-console.log(0.5*2);
+const distance = document.getElementById("distance")
+const consumption = document.getElementById("consumption")
+const fuel = document.getElementById("fuel")
+const button = document.getElementById("calculate")
+const others = document.getElementById("others")
+const result = document.createElement("h2")
+const div = document.getElementById("inner")
+
+
+let distanceValue = null;
+let consumptionValue = null;
+let fuelValue = null;
+let othersValue = null;
+
+others.addEventListener("input", function(e){
+    othersValue = e.target.value;
+})
+
+distance.addEventListener("input", function(e){
+    distanceValue = e.target.value;
+})
+
+consumption.addEventListener("input", function(e){
+    consumptionValue = e.target.value;
+})
+
+fuel.addEventListener("input", function(e){
+    fuelValue = e.target.value;
+})
+
+button.addEventListener("click", function(){
+    result.innerText = `Útiköltség: ${calculateFare()} Ft`
+    div.appendChild(result)
+    
+})
+
+function calculateFare(){
+    return consumptionValue*fuelValue*(distanceValue/100)+othersValue
+}
+
+
+
+
